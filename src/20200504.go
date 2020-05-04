9a9e6eaca5b901095d53c5d60486703a40a89a55
青柳豪 <goaoyagi@Gmini.local>
2020-05-04 21:23:33 +0900

    map try01

--- src/20200504.go
+++ src/20200504.go
@@ -6,6 +6,11 @@ func main() {
 	a := 5
 	var pa *int
 	pa = &a
-	fmt.Println(pa)
-	fmt.Println(*pa)
+	fmt.Println("pointer address &a: " + pa)
+	fmt.Println("pointer value *pa: " + *pa)
+
+	m := map[aaa:true bbb:true]
+	v, ok := m["aaa"]
+	fmt.Println("aaa: " + v + " : " + ok)
+	fmt.Println(m)
 }

8835e6a224147f5eb534fb1fd9b086150b2afec8
青柳豪 <goaoyagi@Gmini.local>
2020-05-04 21:45:31 +0900

    map try06

--- src/20200504.go
+++ src/20200504.go
@@ -10,9 +10,13 @@ func main() {
 	fmt.Println("pointer value *pa:", *pa)
 
 	m := map[string]bool{"aaa": true, "bbb": true}
+	fmt.Println(m)
 	v, ok := m["aaa"]
 	fmt.Println("aaa:", v, ":", ok)
 	v, ok = m["ccc"]
 	fmt.Println("ccc:", v, ":", ok)
-	fmt.Println(m)
+	v = m["bbb"]
+	fmt.Println("bbb:", v)
+	v = m["ddd"]
+	fmt.Println("ddd:", v)
 }
